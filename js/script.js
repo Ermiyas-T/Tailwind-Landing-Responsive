d3ce27ee0e4b14984e4ac27b26eb92f53eda689a
Ermiyas-T <jermiket@gmail.com>
2024-11-25 16:56:38 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//create variables for  click evennts menu
+
+const menu = document.getElementsByClassName("menu-btn")[0];
+const nav = document.getElementsByClassName("nav")[0];
+
+menu.addEventListener("click", function () {
+    menu.classList.toggle("open");
+    // nav.classList.toggle("hidden")
+    nav.classList.toggle("nav-show")
+})
